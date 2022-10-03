d6bdeed2294628326eeff9d5ed10f43fa23674a5
KJ.O <nomeansno@Live.ca>
2022-10-03 14:18:58 +0900

    sevral changes

--- scraper/coupang.py
+++ scraper/coupang.py
@@ -4,25 +4,29 @@ from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+
 from scraper.cp_metadata import DRIVER_PATH
 from scraper.cp_metadata import IMG_SRC_CSS, HREF_CSS, PRODUCT_TITLE_CSS, PRICE_CSS, NEXT_ARROW_BTN_CSS
+
 
 # start_pixel = 1000 #int(input("Start_Pixel : "))
 # end_pixel = 4000 #int(input("End_Pixel : "))
 # driver.execute_script("document.body.style.zoom='25%'")
 # driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
 
-
 def main():
     global count
+    global pageList
 
     print("In progress..")
     build_df()
     for count in range(len(keyword)):
-        build_driver()
-        scroll_range(0, 12000)
-        get_elements()
-        multi_pages()
+        pageList = []
+        for i in range(1, pages + 1):
+            pageList.append(i)
+            build_driver()
+            scroll_range(0, 12000)
+            get_elements()
     df_to_csv()
     driver.quit()
     print("Success!")
@@ -35,25 +39,27 @@ def build_df():
 
 def build_driver():
     global driver
-    url = f"https://www.coupang.com/np/search?rocketAll=false&q={keyword[count]}&brand=&offerCondition=&filter=&availableDeliveryFilter=&filterType=&isPriceRange=false&priceRange=&minPrice=&maxPrice=&page=1&trcid=&traid=&filterSetByUser=true&channel=&backgroundColor=&component=&rating=0&sorter=latestAsc&listSize=72"
+
+    url = f"https://www.coupang.com/np/search?q={keyword[count]}&channel=user&component=&eventCategory=SRP&trcid=&traid=&sorter=latestAsc&minPrice=&maxPrice=&priceRange=&filterType=&listSize=72&filter=&isPriceRange=false&brand=&offerCondition=&rating=0&page={pageList[-1]}&rocketAll=false&searchIndexingToken=&backgroundColor="
     headlessoptions = webdriver.ChromeOptions()
     headlessoptions.add_argument('headless')
-    headlessoptions.add_argument('window-size=1920x1080')
+    headlessoptions.add_argument("window-size=1080x2560")
     headlessoptions.add_argument("disable-gpu")
     headlessoptions.add_argument(
         "User-Agent:  Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36")
     headlessoptions.add_argument("lang=ko_KR")
 
-    #driver = webdriver.Chrome(service=Service(DRIVER_PATH), options=headlessoptions)
+    # driver = webdriver.Chrome(service=Service(DRIVER_PATH), options=headlessoptions)
     driver = webdriver.Chrome(service=Service(DRIVER_PATH))
-    driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {"source": """ Object.defineProperty(navigator, 'webdriver', { get: () => undefined }) """})  # 크롤링 방지 설정을 "undefined"로 변경.
+    driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+        "source": """ Object.defineProperty(navigator, 'webdriver', { get: () => undefined }) """})  # 크롤링 방지 설정을 "undefined"로 변경.
     driver.get(url)
 
 
 def scroll_range(start_pixel, end_pixel):
-    driver.execute_script("document.body.style.transform = 'scale(0.75)'")
+    # driver.execute_script("document.body.style.transform = 'scale(0.25)'")
     time.sleep(2)
-    for pixel in range(start_pixel, end_pixel, 500):
+    for pixel in range(start_pixel, end_pixel, 300):
         driver.execute_script(f"window.scrollTo(0, {pixel})")
         time.sleep(0.5)
 
@@ -78,22 +84,10 @@ def get_elements():
         df_price.append(price_value.text)
 
 
-def next_arrow_btn():
-    next_button = driver.find_element(By.CSS_SELECTOR, NEXT_ARROW_BTN_CSS)
-    next_button.click()
-
-
-def multi_pages():
-    for page in range(pages - 1):
-        next_arrow_btn()
-        scroll_range(0, 12000)
-        get_elements()
-
-
 def df_to_csv():
     df = pd.DataFrame(
         {"detected_date": df_detected_time,
-         "marketplace" : df_marketplace,
+         "marketplace": df_marketplace,
          "img_src": df_img_src,
          "href": df_href,
          "product_title": df_title,
