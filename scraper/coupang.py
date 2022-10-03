f0a15c9cadb4dba95b44a9138ad91e3bb0b930a3
KJ.O <nomeansno@Live.ca>
2022-10-03 15:20:57 +0900

    sevral changes

--- scraper/coupang.py
+++ scraper/coupang.py
@@ -45,6 +45,7 @@ def build_driver():
     headlessoptions.add_argument('headless')
     headlessoptions.add_argument("window-size=1080x2560")
     headlessoptions.add_argument("disable-gpu")
+    headlessoptions.add_argument("no-sandbox")
     headlessoptions.add_argument(
         "User-Agent:  Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36")
     headlessoptions.add_argument("lang=ko_KR")
